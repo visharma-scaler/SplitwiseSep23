558bda787ccc1d917443b8bf833f08213dac91f3
visharma-scaler <vishal.sharma@scaler.com>
2023-12-17 21:44:47 +0530

    [LLD][Machine Coding]: Add Splitwise Requirements + Initial Setup

---
+++ src/main/java/com/scaler/splitwisesep23/SplitwiseSep23Application.java
@@ -0,0 +1,13 @@
+package com.scaler.splitwisesep23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseSep23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseSep23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/splitwisesep23/SplitwiseSep23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.splitwisesep23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseSep23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
